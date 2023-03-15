a7b85bf7b4e912734892dc9388bf5cd1709fb8f9
Firda AR <101695213+firdaauliarakhmah@users.noreply.github.com>
2023-03-15 11:03:14 +0700

    Add files via upload
    PK2

---
+++ PEMPROGRAMAN KOMPUTER 2/p02-Multithreading/src/thread/CurrencyConversionResponse.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thread;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CurrencyConversionResponse {
+    private String base;
+    private String date;
+    private Map<String, String> rates = new TreeMap<>();
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Map<String, String> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, String> rates) {
+        this.rates = rates;
+    }
+}
